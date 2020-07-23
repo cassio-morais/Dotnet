934cc65a74fc99e8e9869c3c63478941ec028a1d
cassio-morais <cassiomoraisg@hotmail.com>
2020-07-23 01:14:08 -0300

    classes estaticas

---
+++ exercicios/ExercicioClassesEstatico/ExercicioClassesEstatico/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioClassesEstatico {
+    class ConversorDeMoeda {
+
+        // public para ser acessado fora da classe
+        // static pq é um método da classe, não precisa de instância para ser usada
+
+        public static double Iof = 0.06;
+        public static double Conversor(double dolar, double quantDolar) {
+
+            dolar = dolar + (dolar * Iof);
+            return dolar * quantDolar;
+        }
+
+    }
+}
---
+++ exercicios/ExercicioClassesEstatico/ExercicioClassesEstatico/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClassesEstatico {
+    class Program {
+        static void Main() {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double Reais = ConversorDeMoeda.Conversor(dolar, quantDolar);
+
+            Console.Write("Valor a ser pago em reais = " + Reais.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
