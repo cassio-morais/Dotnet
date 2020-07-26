890a7cf556ccae82a75d304bd1ce99486aee5ac0
cassio-morais <cassiomoraisg@hotmail.com>
2020-07-26 00:14:05 -0300

    Exercicio vetores

---
+++ exercicios/ExercicioVetores/ExercicioVetores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExercicioVetores {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("How many rooms will be rented? ");
+            int rooms = int.Parse(Console.ReadLine());
+
+            // instanciando um vetor de objetos da classe Renter
+            Renter[] renters = new Renter[10]; 
+
+            for (int i=0; i < rooms; i++) {
+                Console.WriteLine("Rent #"+(i+1));
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                renters[room] = new Renter { Name = name, Email = email, Room = room }; // não criei método construtor
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("Busy rooms: ");
+            for (int r=0; r < renters.Length; r++) { 
+                if (renters[r] != null) {
+                    Console.WriteLine($"{renters[r].Room}: {renters[r].Name}, {renters[r].Email}"); // EXPLÍCITO 
+                }
+            }
+
+
+        }
+    }
+}
---
+++ exercicios/ExercicioVetores/ExercicioVetores/Renter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioVetores {
+    class Renter {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public override string ToString() {
+            return Room + ":" + Name + "," + Email;
+        }
+
+
+        // como o professor implantou
+
+
+        //class Estudante {
+        //    public string Nome { get; set; }
+        //    public string Email { get; set; }
+        //    public Estudante(string nome, string email) {
+        //        Nome = nome;
+        //        Email = email;
+        //    }
+        //    public override string ToString() {
+        //        return Nome + ", " + Email;
+        //    }
+        //}
+
+
+    }
+}
