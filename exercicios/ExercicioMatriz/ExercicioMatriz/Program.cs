fb454a14e3db5c0488f34914d7aee77af3069a32
cassio-morais <cassiomoraisg@hotmail.com>
2020-07-27 14:49:22 -0300

    Exercícios com Matriz

---
+++ exercicios/ExercicioMatriz/ExercicioMatriz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace ExercicioMatriz {
+    class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[x, x];
+
+            int negativo = 0;
+
+            for (int linha=0; linha < x; linha++) {
+                string[] numeros = Console.ReadLine().Split(" ");
+                
+                for (int coluna=0; coluna < x; coluna++) {
+                    matriz[linha, coluna] = int.Parse(numeros[coluna]);
+
+                    if (int.Parse(numeros[coluna]) < 0) {
+                        negativo++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Main Diagonal:");
+            for (int i = 0; i < x; i++) { 
+            Console.Write(matriz[i,i] + " ");
+            
+            }
+
+            Console.WriteLine("\nNegative numbers: " + negativo);
+        }
+
+    }
+}
+
+
+
---
+++ exercicios/ExercicioVetores2/ExercicioVetores2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace ExercicioVetores2 {
+    class Program {
+        static void Main(string[] args) {
+
+            string[] mn = Console.ReadLine().Split(" ");
+
+            int m = int.Parse(mn[0]); // linha
+            int n = int.Parse(mn[1]); // coluna
+
+            int[,] matriz = new int[m, n];
+
+            for (int linha = 0; linha < m; linha++) {
+                string[] valores = Console.ReadLine().Split(" ");
+                for (int coluna = 0; coluna < n; coluna++) {
+                    matriz[linha, coluna] = int.Parse(valores[coluna]);
+                }
+            }
+
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int linha = 0; linha < m; linha++) {
+                for (int coluna = 0; coluna < n; coluna++) {
+                    if (matriz[linha, coluna] == numero) {
+                        Console.WriteLine("Position: " + linha + "," + coluna);
+
+                        if (coluna > 0) {
+                            Console.WriteLine("left: " + matriz[linha, coluna - 1]);
+                        }
+
+                        if (linha > 0) {
+                            Console.WriteLine("up: " + matriz[linha - 1, coluna]);
+                        }
+
+                        if (coluna < m -1) { 
+                            Console.WriteLine("right: " + matriz[linha, coluna + 1]);
+                        }
+
+                        if (linha < n - 1) {
+                            Console.WriteLine("down: " + matriz[linha + 1, coluna]);
+                        }
+
+
+                        
+
+
+                    }
+                }
+            }
+        }
+    }
+}
