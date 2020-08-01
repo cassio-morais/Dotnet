eb36093dbe1b0320df513050b5879ecf740333c8
cassio-morais <cassiomoraisg@hotmail.com>
2020-08-01 01:42:17 -0300

    Manipulando Arquivos**

---
+++ exercicios/ExercArquivos/ExercArquivos/Entities/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace ExercArquivos.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double valor, int quantidade)
+        {
+            Nome = nome;
+            Valor = valor;
+            Quantidade = quantidade;
+        }
+
+        public double Total()
+        {
+            return Valor * Quantidade;
+
+        }
+
+    }
+}
---
+++ exercicios/ExercArquivos/ExercArquivos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using ExercArquivos.Entities;
+
+namespace ExercArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var origem = @"D:\Google Drive\Dotnet\Dotnet\exercicios\ExercArquivos\ExercArquivos\arquivo.txt";
+            var destino = @"D:\Google Drive\Dotnet\Dotnet\exercicios\ExercArquivos\ExercArquivos\out\summary.csv";
+
+            try
+            {
+                string[] arquivoOrigem = File.ReadAllLines(origem);
+
+                foreach (string l in arquivoOrigem)
+                {
+                    var linha = l.Split(',');
+                    var nome = linha[0];
+                    var valor = double.Parse(linha[1], CultureInfo.InvariantCulture);
+                    var quant = int.Parse(linha[2], CultureInfo.InvariantCulture);
+                    
+                    Produto Produto = new Produto(nome, valor, quant);
+                                        
+                    var novaLinha =  Produto.Nome+ "," + Produto.Total().ToString("F2", CultureInfo.InvariantCulture);
+                    
+                    using (var arquivoDestino = File.AppendText(destino))
+                    {
+                        arquivoDestino.WriteLine(novaLinha);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
